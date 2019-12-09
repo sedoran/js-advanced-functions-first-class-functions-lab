2c8bfd6c920c964f04d400ddc9da009e31799c3c
Steven Doran <steven.doran@flatironschool.com>
2019-12-09 17:59:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+  return [drivers[0], drivers[1]]
+}
+
+function returnLastTwoDrivers(drivers) {
+  return [drivers[drivers.length - 2], drivers[drivers.length - 1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return multiplier * fare
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, callback) {
+  return callback(drivers)
+}
